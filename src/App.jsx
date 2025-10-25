f9c5675841715040b6d577bb0385c14d01b76680
sravan7979 <msravan00007@gmail.com>
2025-10-25 12:38:04 +0530

    rotes changed

--- src/App.jsx
+++ src/App.jsx
@@ -19,28 +19,28 @@ import Orders from './components/Orders';
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route index element={<AboutUs />} />
-          <Route path="home" element={<Home />} />
-          <Route path="about-us" element={<AboutUs />} />
-          <Route path="computers" element={<Computers />} />
-          <Route path="mobiles" element={<Mobiles />} />
-          <Route path="laptops" element={<Laptops />} />
-          <Route path="pendrives" element={<Pendrives />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="productpage" element={<ProductPage />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="payment" element={<Payment />} />
-          <Route path="orders" element={<Orders />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter basename="/ecommerce">
+        <Routes>
+          <Route path="/" element={<HomePage />}>
+            <Route index element={<AboutUs />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/computers" element={<Computers />} />
+            <Route path="/mobiles" element={<Mobiles />} />
+            <Route path="/laptops" element={<Laptops />} />
+            <Route path="/pendrives" element={<Pendrives />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/productpage" element={<ProductPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/orders" element={<Orders />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
