3b0657c777370c9d05875ccd831019bae668f5cf
Maturu Sravan Kumar <148370583+sravan7979@users.noreply.github.com>
2025-10-31 22:41:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/ecommerce">
         <Routes>
           <Route path="/" element={<HomePage />}>
             <Route index element={<AboutUs />} />
